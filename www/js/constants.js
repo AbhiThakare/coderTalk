f2c77b58ae8f455696811c961eab6b499ba25477
Abhinav <thakareabhinav@gmail.com>
2016-08-22 12:50:08 +0530

    URL changed

--- www/js/constants.js
+++ www/js/constants.js
@@ -7,5 +7,5 @@ angular.module('app').constant('AUTH_EVENTS', {
     sme: 'ROLE_SME'
 }).constant('URL', {
     url: 'http://169.44.124.140:8091/codertalk/user/',
-    urlChannels: 'http://inmbz2239.in.dst.ibm.com:8091/codertalk/channels/'
+    urlChannels: 'http://169.44.124.140:8091/codertalk/channels/'
 });
